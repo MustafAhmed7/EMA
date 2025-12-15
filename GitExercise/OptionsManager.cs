92a1f088c8fc1ad91536f9459042870cf8114a94
EmreVeysel0 <emre.veysel00@gmail.com>
2025-12-15 11:36:52 +0200

    Add files via upload

---
+++ GitExercise/OptionsManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace GitExercise
+{
+    public static class OptionsManager
+    {
+        public static string[] OptionsList = {
+            "a - Add",
+            "s - Subtract",
+            "m - Multiply",
+            "pow - Power",
+            "log - Logarithm",
+            "fact - Sum of 2 Factorials",
+            "d - Divide",
+            "sabs - Subtract Abs"
+        };
+        public static void Power(double a, double b)
+        {
+            Console.WriteLine($"{a} ^ {b} = {Math.Pow(a, b)}");
+        }
+
+        public static void Log(double a, double b)
+        {
+            Console.WriteLine($"log base {b} of {a} = {Math.Log(a, b)}");
+        }
+
+        public static void Factorial(double a, double b)
+        {
+            double factA = CalculateFact((int)a);
+            double factB = CalculateFact((int)b);
+            Console.WriteLine($"{a}! + {b}! = {(int)a}! + {(int)b}! = {factA + factB}");
+
+        }
+        
+        private static long CalculateFact(int a)
+        {
+            long result = a;
+            for (int i = a - 1; i >= 1; i--)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+        public static void Add(double a, double b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        public static void Multiply(double a, double b)
+        {
+            Console.WriteLine($"{a} x {b} = {a * b}");
+        }
+
+        public static void Subtract(double a, double b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+        public static void Divide(double a, double b)
+        {
+            Console.WriteLine($"{a} : {b} = {a / b}");
+        }
+        public static void SubtractAbs(double a, double b)
+        {
+            Console.WriteLine($"|{a} - {b}| = {Math.Abs(a - b)}");
+        }
+    }
+}
